1c98e25370004a39508e0b885e7cdbe22393b06b
j0 <me@j0.lol>
2024-09-29 20:02:27 +0100

    Some lighthouse optimisations

--- app/fragments/footer.php
+++ app/fragments/footer.php
@@ -5,9 +5,10 @@
         <div class="_88x31s">
             <span class="sr-only">Miscellaneous links:</span>
             <a rel="noreferrer" href="/">
-                <img src="/static/badges/j0.gif" alt="Image contains a purple deer with bright neon yellow eyes, glasses and nose. The neon yellow is flickering. Next to the deer it says in large text: 'j0', and under that it says 'deer thing'. The 'thing' and the slash in the zero both are neon yellow and flicker.">
+                <img width=88 height=31 src="/static/badges/j0.gif" alt="Image contains a purple deer with bright neon yellow eyes, glasses and nose. The neon yellow is flickering. Next to the deer it says in large text: 'j0', and under that it says 'deer thing'. The 'thing' and the slash in the zero both are neon yellow and flicker.">
             </a>
 
-            <a rel="noreferrer" href="https://www.php.net"><img src="/static/badges/php.gif" alt="Powered by PHP"></a>
+            <a rel="noreferrer" href="https://www.php.net">
+                <img width=88 height=31 src="/static/badges/php.gif" alt="Powered by PHP"></a>
         </div>
     </footer>
--- app/fragments/head.php
+++ app/fragments/head.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width">
     <link href="/static/normalize.css" type="text/css" rel="stylesheet">
     <link href="/static/style.css?v=<?php echo md5_file("static/style.css") ?>" type="text/css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css" integrity="sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY=" crossorigin="anonymous">
-    <link rel="stylesheet" href="/static/font-awesome-extension.css">
     <meta name="theme-color" content="#b497ee">
     <meta name="apple-mobile-web-app-status-bar-style" content="#b497ee">
 
