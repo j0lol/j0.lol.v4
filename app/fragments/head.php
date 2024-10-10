e79fad3ec5531ddcc048e02275e5fe7c86be7026
j0 <me@j0.lol>
2024-10-10 20:19:56 +0100

    Favicon

--- app/fragments/head.php
+++ app/fragments/head.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <link href="/static/normalize.css" type="text/css" rel="stylesheet">
-    <link href="/static/style.css?v=<?php echo md5_file("static/style.css") ?>" type="text/css" rel="stylesheet">
+    <link href="/static/style.css?v=<?php echo md5_file(
+        "static/style.css"
+    ); ?>" type="text/css" rel="stylesheet">
     <meta name="theme-color" content="#b497ee">
     <meta name="apple-mobile-web-app-status-bar-style" content="#b497ee">
     <script defer data-domain="j0.lol" src="https://plausible.j0.lol/js/script.js"></script>
@@ -17,28 +19,41 @@
 
         $meta = $posts[$post_slug];
 
-        printf(<<<END
-            <meta property="og:title" content="j0 — %s" />
-        END, $meta["title"]);
+        printf(
+            <<<END
+    <meta property="og:title" content="j0 — %s" />
+END
+            ,
+            $meta["title"]
+        );
 
         // I wish PHP had a way of doing $x = if {} else {} that wasn't ugly (ternary op)
         if (key_exists("subtitle", $meta)) {
-            $description = sprintf("%s — Posted on %s", $meta["subtitle"], $meta["date"]);
+            $description = sprintf(
+                "%s — Posted on %s",
+                $meta["subtitle"],
+                $meta["date"]
+            );
         } else {
             $description = sprintf("Posted on %s", $meta["date"]);
         }
 
-        printf(<<<END
-            <meta property="og:description" content="%s" />
-        END, $description);
-
+        printf(
+            <<<END
+    <meta property="og:description" content="%s" />
+END
+            ,
+            $description
+        );
     } else {
         echo <<<END
-            <meta property="og:title" content="j0" />
-        END;
+    <meta property="og:title" content="j0" />
+END;
     }
     ?>
 
+    <meta rel="icon" content="https://j0.lol/static/j0site-pfp.png"/>
+    <meta rel="apple-touch-icon" content="https://j0.lol/static/j0site-pfp.png"/>
     <meta property="og:image" content="https://j0.lol/static/j0site-banner.png"/>
 
     <style>
