f68fd1c3eb019d8354404fba7486cd01db056451
j0 <me@j0.lol>
2024-10-10 22:31:37 +0100

    Reformat 88x31 wall

--- app/88x31-wall.php
+++ app/88x31-wall.php
@@ -1,9 +1,10 @@
 <?php global $router;
-fragment("head") ?>
+fragment("head");
+?>
 
 
 <div class="wrapper" xmlns="http://www.w3.org/1999/html">
-    <?php fragment("navbar") ?>
+    <?php fragment("navbar"); ?>
 
     <main>
         <h1>88x31 Button Wall</h1>
@@ -31,10 +32,10 @@ fragment("head") ?>
         <pre class="fakepre">
             <?php
             $string = <<<END
-                <a rel="noreferrer" href="https://j0.lol">
-                    <img src="https://j0.lol/static/badges/j0.gif" alt="Image contains a purple deer with bright neon yellow eyes, glasses and nose. The neon yellow is flickering. Next to the deer it says in large text: 'j0', and under that it says 'deer thing'. The 'thing' and the slash in the zero both are neon yellow and flicker.">
-                </a>
-                END;
+<a rel="noreferrer" href="https://j0.lol">
+    <img src="https://j0.lol/static/badges/j0.gif" alt="Image contains a purple deer with bright neon yellow eyes, glasses and nose. The neon yellow is flickering. Next to the deer it says in large text: 'j0', and under that it says 'deer thing'. The 'thing' and the slash in the zero both are neon yellow and flicker.">
+</a>
+END;
 
             echo htmlspecialchars($string);
             ?>
@@ -48,12 +49,23 @@ fragment("head") ?>
         <p>These are the buttons that my friends made to link to their websites. Click them to go to them!</p>
         <div class="_88x31s">
             <?php
-
             $buttons = [
-                "meihapps.gif" => ["Box with purple border and dark indigo background. Text says 'mei happs' in monospaced font.", "https://meihapps.gay"],
-                "finn.gif" => ["Green striped background. Handwritten, pixelated text says 'cr0wbar'. The 0 has what appears to be a shiny gem inside of it.", "https://https://cr0wbarsartdump.neocities.org/"],
-                "cadence_now.png" => ["Purple background. Text says cadence Now!. The first part of the text is in an 'outrun' vaporwave-type style. The final part is handwritten. There is what appears to be a purple eye with a yellow iris to the left of the text. Enclosing the eye is a white letter c. There is a star banner on the bottom right.", "https://cadence.moe"],
-                "barrow.png" => ["Green text says 'Join Barr0wnet'. Enclosing the text is a scientific diagram of the chemical alpha-methyl.", "http://alphamethyl.barr0w.net"],
+                "meihapps.gif" => [
+                    "Box with purple border and dark indigo background. Text says 'mei happs' in monospaced font.",
+                    "https://meihapps.gay",
+                ],
+                "finn.gif" => [
+                    "Green striped background. Handwritten, pixelated text says 'cr0wbar'. The 0 has what appears to be a shiny gem inside of it.",
+                    "https://cr0wbar.dev",
+                ],
+                "cadence_now.png" => [
+                    "Purple background. Text says cadence Now!. The first part of the text is in an 'outrun' vaporwave-type style. The final part is handwritten. There is what appears to be a purple eye with a yellow iris to the left of the text. Enclosing the eye is a white letter c. There is a star banner on the bottom right.",
+                    "https://cadence.moe",
+                ],
+                "barrow.png" => [
+                    "Green text says 'Join Barr0wnet'. Enclosing the text is a scientific diagram of the chemical alpha-methyl.",
+                    "http://alphamethyl.barr0w.net",
+                ],
             ];
 
             foreach ($buttons as $file => [$alt, $link]) {
@@ -64,12 +76,10 @@ fragment("head") ?>
                     $alt
                 );
             }
-
             ?>
 
         </div>
 
     </main>
-    <?php fragment("footer") ?>
+    <?php fragment("footer"); ?>
 </div>
-
