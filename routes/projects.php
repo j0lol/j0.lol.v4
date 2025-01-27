73baf82bb5658965fc9d8c56affef3d6f63f1814
j0 <me@j0.lol>
2025-01-27 12:12:39 +0000

    Add Sokobubble project & play buttons

--- routes/projects.php
+++ routes/projects.php
@@ -14,40 +14,42 @@ fragment("head") ?>
 
             <?php
 
-            # [ Title, Description, URL, Year, Image slug ]
+            # [ Title, Description, URL, Year, Image slug, Playable URL ]
             $cards = array(
-                ["mel0n", "🚧 sphere-stacker game for the GBA. Rust & agb-rs.", 'https://github.com/j0lol/mel0n', '2024', 'mel0n.jpg'],
-                ["Dig", "🚧 Side-view 2D Platformer with Building. Rust & Macroquad. Click to play! (Keyboard + Mouse)", 'https://vps.j0.lol/dig/', '2024', 'dig.jpg'],
-                ["ZeroBridge", "Discord bridge for Minecraft 1.4.7. NilLoader mod.", 'https://github.com/j0lol/ZeroBridge', '2024', "zerobridge.jpg"],
-                ["World Conquest", "Group project: Risk clone made in Godot. I was project lead.", 'https://github.com/goblin-code-se/world_conquest', '2024', "conquest.jpg"],
-                ["TIC-80 Tetris", "Physics-accurate Tetris clone for the TIC-80 Fantasy console. Click to play! (Keyboard only)", 'https://vps.j0.lol/tetr/', '2023', "tetr.jpg"],
-                ["launCCher", "ToonTown: Corporate Clash launcher for Linux. Rust & egui.", 'https://codeberg.org/j0/launccher', '2023', "launccher.jpg"],
-                ["Modulus", "🚧 Modular tool mod for Minecraft. Kotlin, Quilt mod.", 'https://github.com/j0lol/modulus', '2022-2024', "modulus.png"],
-                ["Quicksnad", "Simple Fabric Minecraft mod that lets you grow cacti & sugarcane quicker. Java.", 'https://modrinth.com/mod/quicksnad', '2022', "snad.png"],
-                ["TransVoiceParty", "List of trans voice training resources.", 'https://transvoice.party', '2019-now', "tvp.jpg"],
+                ["Sokobubble", "Block-pushing (via bubbles) game for Global Game Jam 2025", 'https://github.com/j0lol/ggj25', '2025', 'sokobubble.jpg', null],
+                ["mel0n", "sphere-stacker game for the GBA. Rust & agb-rs.", 'https://github.com/j0lol/mel0n', '2024 🚧', 'mel0n.jpg', null],
+                ["Dig", "Side-view 2D Platformer with Building. Rust & Macroquad. Click to play! (Keyboard + Mouse)", 'https://github.com/j0lol/dig-v2', '2024 🚧', 'dig.jpg', 'https://vps.j0.lol/dig/'],
+                ["ZeroBridge", "Discord bridge for Minecraft 1.4.7. NilLoader mod.", 'https://github.com/j0lol/ZeroBridge', '2024', "zerobridge.jpg", null],
+                ["World Conquest", "Group project: Risk clone made in Godot. I was project lead.", 'https://github.com/goblin-code-se/world_conquest', '2024', "conquest.jpg", 'https://vps.j0.lol/conquest'],
+                ["TIC-80 Tetris", "Physics-accurate Tetris clone for the TIC-80 Fantasy console. Click to play! (Keyboard only)", null, '2023', "tetr.jpg",'https://vps.j0.lol/tetr/'],
+                ["launCCher", "ToonTown: Corporate Clash launcher for Linux. Rust & egui.", 'https://codeberg.org/j0/launccher', '2023', "launccher.jpg", null],
+                ["Modulus", "Modular tool mod for Minecraft. Kotlin, Quilt mod.", 'https://github.com/j0lol/modulus', '2022-2024 🚧', "modulus.png", null],
+                ["Quicksnad", "Simple Fabric Minecraft mod that lets you grow cacti & sugarcane quicker. Java.", 'https://modrinth.com/mod/quicksnad', '2022', "snad.png", null],
+                ["TransVoiceParty", "List of trans voice training resources.", 'https://transvoice.party', '2019-now', "tvp.jpg", null],
             );
 
-            foreach ($cards as [$name, $description, $url, $year, $imslug]) {
-                printf('<a href="%s" style="text-decoration: none">
+            foreach ($cards as [$name, $description, $url, $year, $imslug, $play]) {
+                ?>
                     <div class="card">
                         <div class="preview">
-                            <img class="raw" src="%s" alt="Screenshot of project.">
+                            <img class="raw" src="<?= "/static/card_images/" . $imslug ?>" alt="Screenshot of project.">
                         </div>
                         <div class="blurb">
-                            <span class="title">%s
-                                <span class="year">%s</span>
+                            <span class="title"><?= $name ?>
+                                <span class="year"><?= $year ?></span>
                             </span>
-                            <span class="description">%s</span>
+                            <span class="description"><?= $description ?></span>
+                        </div>
+                        <div class="links">
+                            <?php if ($play != null) {
+                                ?> <a href="<?= $play ?>">Play!</a> <?php
+                            }?>
+                            <?php if ($url != null) {
+                                ?> <a href="<?= $url ?>">Source</a> <?php
+                            }?>
                         </div>
                     </div>
-                </a>
-                ',
-                    $url,
-                    "/static/card_images/" . $imslug,
-                    $name,
-                    $year,
-                    $description
-                );
+                <?php
             }
             ?>
         </div>
