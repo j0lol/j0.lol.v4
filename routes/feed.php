688975ce5271e5201af5588cd51955a71fbd5587
j0 <me@j0.lol>
2025-03-26 20:05:16 +0000

    add comments to my site

--- routes/feed.php
+++ routes/feed.php
@@ -1,6 +1,5 @@
 <?php
 
-use Michelf\MarkdownExtra;
 require_once "remote/atom.gen.php"; # https://starbeamrainbowlabs.com/code/phpatomgenerator/
 global $posts;
 $host = "https://j0.lol"; // So this doesn't break when im working on localhost
@@ -10,7 +9,7 @@ $feed->title = "j0's blog";
 $feed->id_uri = $host . "/blog";
 $feed->feed_uri = $host . "/feed";
 $feed->logo_uri = $host . "/static/j0site-pfp.png";
-$feed->addauthor("Jo Null", "me@j0.lol", "https://j0.lol/", "author");
+$feed->addauthor("Jo Null", "me@j0.lol", "https://j0.lol/");
 
 foreach ($posts as $slug => $post) {
     $content = file_get_contents("./posts/" . $slug . ".html");
